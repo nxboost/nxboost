3ac15b0a6e76b4561a904e770243789795d3032c
warrows <warrows@gamer666.fr>
2018-08-30 15:29:46 +0200

    Add an address field to the receive form
    Users will more easily access to an unused address if it's readily
    available with no click needed

--- src/qt/receivecoinsdialog.h
+++ src/qt/receivecoinsdialog.h
@@ -60,6 +60,8 @@ private:
     GUIUtil::TableViewLastColumnResizingFixer* columnResizingFixer;
     WalletModel* model;
     QMenu* contextMenu;
+
+    QString getAddress(QString label = "");
     void copyColumnToClipboard(int column);
     virtual void resizeEvent(QResizeEvent* event);
 
