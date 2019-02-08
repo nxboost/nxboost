e351083fa9f231da9740ec1efd9f609ec35c3c15
random-zebra <random.zebra@protonmail.com>
2019-02-08 12:11:57 +0100

    remove extra debug lines in AcceptBlock

--- src/main.cpp
+++ src/main.cpp
@@ -4393,13 +4393,11 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
 
             // Now that this loop if completed. Check if we have zNXB inputs.
             if(hasZNXBInputs){
-                LogPrintf("It has ZNXB Inputs!!\n");
                 for (CTxIn zNXBInput : zNXBInputs) {
                     CoinSpend spend = TxInToZerocoinSpend(zNXBInput);
 
                     // First check if the serials were not already spent on the forked blocks.
                     CBigNum coinSerial = spend.getCoinSerialNumber();
-                    LogPrintf("coinSerial is %s\n", coinSerial.ToString(10));
                     for(CBigNum serial : vBlockSerials){
                         if(serial == coinSerial){
                             return state.DoS(100, error("%s: serial double spent on fork", __func__));
@@ -4410,12 +4408,9 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                     int nHeightTx = 0;
                     if (IsSerialInBlockchain(spend.getCoinSerialNumber(), nHeightTx)){
                         // if the height is nHeightTx > chainSplit means that the spent occurred after the chain split
-                        LogPrintf("Here nHeightTx is %d while splitHeight %d\n", nHeightTx, splitHeight);
-                        if(nHeightTx <= splitHeight){
+                        if(nHeightTx <= splitHeight)
                             return state.DoS(100, error("%s: serial double spent on main chain", __func__));
-                        }
                     }
-                    LogPrintf("But passes the checks\n", coinSerial.ToString(10));
 
                     if (!ContextualCheckZerocoinSpendNoSerialCheck(stakeTxIn, spend, pindex, 0))
                         return state.DoS(100,error("%s: ContextualCheckZerocoinSpend failed for tx %s", __func__,
