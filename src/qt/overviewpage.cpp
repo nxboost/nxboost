eons-finance/ALQO

Simplify watch only balance display logic
Instead of making the watch-only balances independent, make them inherit
 their visible state from non watch-only balances. This prevents the
 situation where a watch-only line item is blank.

src/qt/overviewpage.cpp
@@ -281,28 +281,28 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
 
     // PIV Available
     bool showPIVAvailable = settingShowAllBalances || pivAvailableBalance != nTotalBalance;
-    bool showWatchOnlyPIVAvailable = settingShowAllBalances || nAvailableWatchBalance != nTotalWatchBalance;
+    bool showWatchOnlyPIVAvailable = showPIVAvailable || nAvailableWatchBalance != nTotalWatchBalance;
     ui->labelBalanceText->setVisible(showPIVAvailable || showWatchOnlyPIVAvailable);
     ui->labelBalance->setVisible(showPIVAvailable || showWatchOnlyPIVAvailable);
     ui->labelWatchAvailable->setVisible(showWatchOnlyPIVAvailable && showWatchOnly);
 
     // PIV Pending
     bool showPIVPending = settingShowAllBalances || unconfirmedBalance != 0;
-    bool showWatchOnlyPIVPending = settingShowAllBalances || watchUnconfBalance != 0;
+    bool showWatchOnlyPIVPending = showPIVPending || watchUnconfBalance != 0;
     ui->labelPendingText->setVisible(showPIVPending || showWatchOnlyPIVPending);
     ui->labelUnconfirmed->setVisible(showPIVPending || showWatchOnlyPIVPending);
     ui->labelWatchPending->setVisible(showWatchOnlyPIVPending && showWatchOnly);
 
     // PIV Immature
     bool showPIVImmature = settingShowAllBalances || immatureBalance != 0;
-    bool showWatchOnlyImmature = settingShowAllBalances || watchImmatureBalance != 0;
+    bool showWatchOnlyImmature = showPIVImmature || watchImmatureBalance != 0;
     ui->labelImmatureText->setVisible(showPIVImmature || showWatchOnlyImmature);
     ui->labelImmature->setVisible(showPIVImmature || showWatchOnlyImmature); // for symmetry reasons also show immature label when the watch-only one is shown
     ui->labelWatchImmature->setVisible(showWatchOnlyImmature && showWatchOnly); // show watch-only immature balance
 
     // PIV Locked
     bool showPIVLocked = settingShowAllBalances || nLockedBalance != 0;
-    bool showWatchOnlyPIVLocked = settingShowAllBalances || nWatchOnlyLockedBalance != 0;
+    bool showWatchOnlyPIVLocked = showPIVLocked || nWatchOnlyLockedBalance != 0;
     ui->labelLockedBalanceText->setVisible(showPIVLocked || showWatchOnlyPIVLocked);
     ui->labelLockedBalance->setVisible(showPIVLocked || showWatchOnlyPIVLocked);
     ui->labelWatchLocked->setVisible(showWatchOnlyPIVLocked && showWatchOnly);

nxboost/nxboost

src/qt/overviewpage.cpp
@@ -265,28 +265,28 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
 
     // NXB Available
     bool showNXBAvailable = settingShowAllBalances || NXBAvailableBalance != nTotalBalance;
-    bool showWatchOnlyNXBAvailable = settingShowAllBalances || nAvailableWatchBalance != nTotalWatchBalance;
+    bool showWatchOnlyNXBAvailable = showNXBAvailable || nAvailableWatchBalance != nTotalWatchBalance;
     ui->labelBalanceText->setVisible(showNXBAvailable || showWatchOnlyNXBAvailable);
     ui->labelBalance->setVisible(showNXBAvailable || showWatchOnlyNXBAvailable);
     ui->labelWatchAvailable->setVisible(showWatchOnlyNXBAvailable && showWatchOnly);
 
     // NXB Pending
     bool showNXBPending = settingShowAllBalances || unconfirmedBalance != 0;
-    bool showWatchOnlyNXBPending = settingShowAllBalances || watchUnconfBalance != 0;
+    bool showWatchOnlyNXBPending = showNXBPending || watchUnconfBalance != 0;
     ui->labelPendingText->setVisible(showNXBPending || showWatchOnlyNXBPending);
     ui->labelUnconfirmed->setVisible(showNXBPending || showWatchOnlyNXBPending);
     ui->labelWatchPending->setVisible(showWatchOnlyNXBPending && showWatchOnly);
 
     // NXB Immature
     bool showNXBImmature = settingShowAllBalances || immatureBalance != 0;
-    bool showWatchOnlyImmature = settingShowAllBalances || watchImmatureBalance != 0;
+    bool showWatchOnlyImmature = showNXBImmature || watchImmatureBalance != 0;
     ui->labelImmatureText->setVisible(showNXBImmature || showWatchOnlyImmature);
     ui->labelImmature->setVisible(showNXBImmature || showWatchOnlyImmature); // for symmetry reasons also show immature label when the watch-only one is shown
     ui->labelWatchImmature->setVisible(showWatchOnlyImmature && showWatchOnly); // show watch-only immature balance
 
     // NXB Locked
     bool showNXBLocked = settingShowAllBalances || nLockedBalance != 0;
-    bool showWatchOnlyNXBLocked = settingShowAllBalances || nWatchOnlyLockedBalance != 0;
+    bool showWatchOnlyNXBLocked = showNXBLocked || nWatchOnlyLockedBalance != 0;
     ui->labelLockedBalanceText->setVisible(showNXBLocked || showWatchOnlyNXBLocked);
     ui->labelLockedBalance->setVisible(showNXBLocked || showWatchOnlyNXBLocked);
     ui->labelWatchLocked->setVisible(showWatchOnlyNXBLocked && showWatchOnly);