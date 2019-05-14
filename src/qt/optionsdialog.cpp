c063c73c86d2063e7f45b6fd2fac5bfa3c369079
Alko89 <alefer89@gmail.com>
2019-05-14 11:48:14 +0200

    Hide znxb related options.

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -69,9 +69,12 @@ OptionsDialog::OptionsDialog(QWidget* parent, bool enableWallet) : QDialog(paren
     ui->tabWidget->removeTab(ui->tabWidget->indexOf(ui->tabWindow));
 #endif
 
-    /* remove Wallet tab in case of -disablewallet */
+    /* remove Wallet tab and zNXB options in case of -disablewallet */
     if (!enableWallet) {
         ui->tabWidget->removeTab(ui->tabWidget->indexOf(ui->tabWallet));
+
+        ui->verticalZpivOptionsWidget->hide();
+        ui->verticalZpivDisplayWidget->hide();
     }
 
     /* Display elements init */
